96dc167b3812ed115e345dc15c5ab90f0a4a7805
Blue-Fire <blue_fire@icloud.com>
2014-02-11 20:42:28 +0100

    Ajout de la classe Personnage
    Elle définie un personnage joueur ou IA

---
+++ src/Personnage.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Romain
+ * Définie un personnage ( Joueur ou IA ) avec de la vie du mana et un niveau.
+ */
+public class Personnage {
+
+	/**
+	 * Vie par defaut
+	 */
+	public static final int DEFAULT_LIFE = 100;
+	public static final int DEFAULT_MANA = 100;
+	public static final int DEFAULT_LEVEL = 1;
+	
+}
