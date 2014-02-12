9f4dc907f9a8e43cf1adf2a83465eccde33da222
Blue-Fire <blue_fire@icloud.com>
2014-02-12 19:36:10 +0100

    Suppression avertissement doc
    Par ce que c'est énervant tous ces avertissements et je ne me résigne
    pas à l'enlever des paramètres.

--- src/main.java
+++ src/main.java
@@ -1,10 +1,10 @@
 
+@SuppressWarnings("javadoc")
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		// TODO Auto-generated method stub
-			Personnage romain;
-			romain = new Personnage();
+			Personnage romain = new Personnage();
 			System.out.println("Coucou");
 	}
 
